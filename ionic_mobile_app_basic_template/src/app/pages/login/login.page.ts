81bed17adfb7f733e20ded64d7296383aa6633f8
andresbpaz <andabopa@gmail.com>
2023-06-09 18:05:44 -0500

    copy

--- ionic_mobile_app_basic_template/src/app/pages/login/login.page.ts
+++ ionic_mobile_app_basic_template/src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+//pagina de login para el usuario
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  errors: any;
+  username: string = '';
+  password: string = '';
+  form: any;
+  path: any = 'account';
 
-  ngOnInit() {
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder
+    ) { 
+      this.form = this.fb.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+    }
+    
+
+  ngOnInit() {        
+    this.path = this.navParams.data.path;
+  }
+
+  forgotton() {
+    throw new Error('Method not implemented.');
+  }
+
+  login() {
+    if (this.username && this.password) {
+      /*this.authService.login(this.username, this.password).subscribe(
+        () => {
+          // Login exitoso, redireccionar a la página principal
+        },
+        (error) => {
+          // Manejar el error de autenticación
+          console.error(error);
+        }
+      );*/
+    }
+    throw new Error('Method not implemented.');
   }
 
 }
--- ionic_mobile_app_basic_template/src/app/services/auth.service.ts
+++ ionic_mobile_app_basic_template/src/app/services/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = 'https://api.example.com'; // URL de la API de autenticación
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  login(username: string, password: string): Observable<any> {
+    const loginData = {
+      username: username,
+      password: password
+    };
+    return this.http.post(`${this.apiUrl}/login`, loginData);
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/logout`, {});
+  }
 }
